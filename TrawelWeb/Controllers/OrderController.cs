02d7471d045dfb39c4f29d18aedf91ef8e9c58bf
kubilay13 <145432835+kubilay13@users.noreply.github.com>
2023-12-22 16:40:50 +0300

    x

---
+++ TrawelWeb/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrawelWeb.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
